623007007e236ec4133bac1f31eee07b04733adc
Petter Persson <petter.p.persson@gmail.com>
2026-01-21 15:30:23 +0100

    example script on how to run on hpc2n AMD node

--- job_scripts/run_time_test.sh
+++ job_scripts/run_time_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Here you should put your own project id
 #SBATCH -A hpc2n2026-031
-# Allocate GPU and 14 CPU cores
-#SBATCH --gres=gpu:v100:1
+# Ask for 1 Nvidia l40s gpu node
+#SBATCH --gpus-per-node=l40s:1
 #SBATCH -c 14
 # Ask for a suitable amount of time. Remember, this is the time the Jupyter notebook will be available!
 #SBATCH --time=00:05:00
@@ -10,12 +10,14 @@
 #SBATCH -J ml_pytorch
 # Send mail when job begins
 #SBATCH --mail-type=BEGIN
- 
+
 # Clear the environment from any previously loaded modules
 module purge > /dev/null 2>&1
- 
+
 # Load the module environment suitable for the job
-ml restore petter_modules
+ml GCCcore/12.3.0 GCC/12.3.0 OpenMPI/4.1.5
+ml PyTorch-bundle/2.1.2-CUDA-12.1.1
+ml Arrow/14.0.1 PyYAML/6.0 matplotlib/3.7.2 tensorboard/2.15.1
 
 # Run WGANGP with FCGAN with architecture
 ./run_wgan_gp_runtime_test.sh
