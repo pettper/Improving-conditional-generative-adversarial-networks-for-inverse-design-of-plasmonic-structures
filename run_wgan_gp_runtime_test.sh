41998802a221c3d410e86ecf3e800e615e410c85
Petter Persson <petter.p.persson@gmail.com>
2026-02-11 09:51:46 +0100

    allows for passing settings as argument for runtime test

--- run_wgan_gp_runtime_test.sh
+++ run_wgan_gp_runtime_test.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=$PYTHONPATH:.
 
-python tests/performance_tests/wgan_gp_runtime_test.py -s settings/benchmarking/dcgan.yaml
+python tests/performance_tests/wgan_gp_runtime_test.py -s $1
